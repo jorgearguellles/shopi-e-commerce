66d3537dca27d91f0f90f3cfe5937d709d305522
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2025-01-22 14:51:31 -0500

    [Add] Product Card Component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,5 +1,12 @@
+import { Card } from "../../Components/Card";
 import { Layout } from "../../Components/Layout";
 
 export const Home = () => {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      Home
+      <Card />
+      <Card />
+    </Layout>
+  );
 };
